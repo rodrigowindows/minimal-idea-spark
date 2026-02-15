6dfa67f8e2e9d70b1b581c335697af8bddc00739
rodrigowindows <rodrigowindows@gmail.com>
2026-02-15 18:53:27 -0500

    chore: remove 19 unused UI components and 14 unused dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -265,7 +264,6 @@ function AuthGate() {
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <NightWorkerProvider>
         <BrowserRouter>
